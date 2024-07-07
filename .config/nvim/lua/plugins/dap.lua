93fa3592b2e93974039be00d52f2cebdd95e2997
aura-zero <akyv2.5@gmail.com>
2024-07-07 22:07:27 +0530

    neovim config > onsidian setup

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,5 +1,4 @@
 require('dapui').setup()
-require('dap-go').setup()
 require('nvim-dap-virtual-text').setup()
 vim.fn.sign_define('DapBreakpoint', { text='🔴', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 
