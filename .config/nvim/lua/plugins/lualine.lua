630c2eb7508594a71ea88ebccf0bf24e81b475df
Abhishek Yadav <akyv2.5@gmail.com>
2024-11-02 15:45:40 +0530

    nvim and tmux fixes

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,27 +1,15 @@
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    component_separators = '|',
-    section_separators = '',
-  },
-  sections = {
-    lualine_x = {
-      {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      },
-      {
-        require("noice").api.status.command.get,
-        cond = require("noice").api.status.command.has,
-        color = { fg = "#ff9e64" },
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = "VeryLazy",
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
       },
     },
-    lualine_a = {
-      {
-        'buffers',
-      }
-    }
-  }
+  },
 }
-
