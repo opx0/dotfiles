01033c8f394defc4adafb3d194d3c7729e57eaf7
Abhishek Yadav <akyv2.5@gmail.com>
2024-12-20 22:58:59 +0530

    zen mode waybar config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -2,7 +2,8 @@ require("obsidian").setup({
   workspaces = {
     {
       name = "Notes",
-      path = "/home/abhi/DATA /Archives/DevLogs",
+      -- path = "/home/abhi/DATA /Archives/DevLogs",
+      path = "/home/susanoo/GrandLine/Archives/DevLogs/",
     },
   },
 })
