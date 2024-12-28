11f2e32e5bc14dd995065399390b74c6e2280b90
Abhishek Yadav <akyv2.5@gmail.com>
2024-12-28 17:05:10 +0530

    added nu shell | [200~mkdir ~/.cache/starship

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,18 +1,17 @@
 local wezterm = require 'wezterm'
 local gpus = wezterm.gui.enumerate_gpus()
---
---if os.getenv("XDG_CURRENT_DESKTOP") == "Hyprland" then
---	config.enable_wayland = false
---else
---	config.enable_wayland = true
---end
+
 return {
 	enable_wayland = false,
+	adjust_window_size_when_changing_font_size = false,
 	-- color_scheme = 'termnial.sexy',
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
-	font_size = 13.0,
-	font = wezterm.font('JetBrainsMono Nerd Font'),
+	font_size = 14.0,
+	font = wezterm.font('JetBrains Mono'),
+	-- macos_window_background_blur = 40,
+	macos_window_background_blur = 30,
+
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
 	-- 	brightness = 0.01,
@@ -26,9 +25,14 @@ return {
 	window_decorations = 'RESIZE',
 	keys = {
 		{
-			key = 'f',
+			key = 'q',
 			mods = 'CTRL',
 			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = '\'',
+			mods = 'CTRL',
+			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
 		},
 	},
 	mouse_bindings = {
@@ -39,6 +43,4 @@ return {
 	    action = wezterm.action.OpenLinkAtMouseCursor,
 	  },
 	},
-	-- Spawn a tmux  in login mode
-	--default_prog = { "tmux",'-l'} --big flaw:> creating new session but it should attach
 }
